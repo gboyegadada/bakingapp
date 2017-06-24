350565c82e0a5acdc7fa3d902ec14a63f6250c0d
Gboyega Dada <gboyega.dada@saswarenigeria.com>
2017-06-24 22:59:01 +0100

    minor change

--- app/src/androidTest/java/com/example/android/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/MainActivityTest.java
@@ -1,52 +1,1 @@
-package com.example.android.bakingapp;
-
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
-
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule =
-            new ActivityTestRule<>(MainActivity.class);
-
-
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.android.bakingapp", appContext.getPackageName());
-    }
-
-
-    /*
-    @Test
-    public void ensureStepListActivityIsDisplayed() {
-        // Click item in Recycler View
-        onView(withId(R.id.rv_recipes))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, MyViewAction.clickChildViewWithId(R.id.tv_recipe_name)));
-    }
-    */
-}
+package com.example.android.bakingapp;import android.content.Context;import android.support.test.InstrumentationRegistry;import android.support.test.espresso.contrib.RecyclerViewActions;import android.support.test.rule.ActivityTestRule;import android.support.test.runner.AndroidJUnit4;import org.junit.Rule;import org.junit.Test;import org.junit.runner.RunWith;import static android.support.test.espresso.Espresso.onView;import static android.support.test.espresso.action.ViewActions.click;import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;import static android.support.test.espresso.action.ViewActions.typeText;import static android.support.test.espresso.assertion.ViewAssertions.matches;import static android.support.test.espresso.matcher.ViewMatchers.withId;import static android.support.test.espresso.matcher.ViewMatchers.withText;import static org.junit.Assert.assertEquals;/** * Instrumentation test, which will execute on an Android device. * * @see <a href="http://d.android.com/tools/testing">Testing documentation</a> */@RunWith(AndroidJUnit4.class)public class MainActivityTest {    @Rule    public ActivityTestRule<MainActivity> mActivityRule =            new ActivityTestRule<>(MainActivity.class);    @Test    public void useAppContext() throws Exception {        // Context of the app under test.        Context appContext = InstrumentationRegistry.getTargetContext();        assertEquals("com.example.android.bakingapp", appContext.getPackageName());    }    /*    @Test    public void ensureStepListActivityIsDisplayed() {        // Click item in Recycler View        onView(withId(R.id.rv_recipes))                .perform(RecyclerViewActions.actionOnItemAtPosition(1, MyViewAction.clickChildViewWithId(R.id.tv_recipe_name)));    }    */}
